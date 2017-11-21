bad6069d87f75372e998a664ca1acaf00222b071
Ben Yanke <ben@benyanke.com>
2017-11-20 18:59:21 -0600

    Provide defaults
    Discussed in #2

--- app/code/local/Rossigee/Core/Model/Config/Element.php
+++ app/code/local/Rossigee/Core/Model/Config/Element.php
@@ -12,9 +12,38 @@ class Rossigee_Core_Model_Config_Element extends Mage_Core_Model_Config_Element
     public function xmlentities($value = null)
     {
         $value = parent::xmlentities($value);
+        
+        // If this variable has a $ in the first char, use an env var
         if(substr($value, 0, 1) == "$") {
-            $value = getenv(substr($value, 1));
+
+            // If || is in the string, parse it correctly to handle defaults
+            $defaultCharPos = strpos($value, '||');
+            if ($defaultCharPos !== false) {
+
+                // If env var exists, use it, parsing out the default values
+                $envVarName = substr($value, 1, $defaultCharPos-1);
+                $defaultValue = substr($value, $defaultCharPos-1);
+
+                // Use whichever of these work for you, or maybe leave it as server if that works?
+                // getenv() didn't work for me, but $_SERVER did
+                
+                $envValue = $_SERVER[$envVarName];
+                // $envValue = getenv($envVarName);
+
+                if($envValue) {
+                        return $envValue;
+
+                // If it doesn't, use default value provided
+                } else {
+                        return $defaultValue;
+                }
+
+            // No defaults, use the full value no matter what
+            } else {
+                return getenv(substr($value, 1));
+            }
+
         }
-        return $value;
+        
     }
 }
